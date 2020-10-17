cf9991179497a0fa33ed423cad43b283192b02d5
marzeq <marzeqmarzeq@gmail.com>
2020-10-17 22:16:30 +0200

    Command doesnt exist response

--- code/cogs/help.py
+++ code/cogs/help.py
@@ -27,19 +27,23 @@ class HelpCommand(commands.Cog):
             response_embed.set_footer(text="[] are required command arguments, () are optional command arguments")
             await ctx.send(embed=response_embed)
         else:
-            response_embed = discord.Embed(title=f"**The {command} command:**", color=0x1ced23)
-            args_to_put = ""
-            for arg in self.dsc[command]["args"]:
-                if self.dsc[command]["args"][arg]["required"]:
-                    args_to_put += f" [{arg}]"
-                else:
-                    args_to_put += f" ({arg})"
-            response_embed.add_field(name=f"**Usage:**", value=f"```{command}{args_to_put}```")
-            response_embed.add_field(name=f"**Description:**", value=f"```{self.dsc[command]['desc']}```")
-            response_embed.add_field(name=f"**Required Permissions:**", value=f"```{self.dsc[command]['required_perms']}```")
-            response_embed.add_field(name=f"**Aliases:**", value=f"```{self.dsc[command]['aliases']}```")
-            response_embed.set_footer(text="[] are required command arguments, () are optional command arguments")
-            await ctx.send(embed=response_embed)
+            try:
+                response_embed = discord.Embed(title=f"**The {command} command:**", color=0x1ced23)
+                args_to_put = ""
+                for arg in self.dsc[command]["args"]:
+                    if self.dsc[command]["args"][arg]["required"]:
+                        args_to_put += f" [{arg}]"
+                    else:
+                        args_to_put += f" ({arg})"
+                response_embed.add_field(name=f"**Usage:**", value=f"```{command}{args_to_put}```")
+                response_embed.add_field(name=f"**Description:**", value=f"```{self.dsc[command]['desc']}```")
+                response_embed.add_field(name=f"**Required Permissions:**", value=f"```{self.dsc[command]['required_perms']}```")
+                response_embed.add_field(name=f"**Aliases:**", value=f"```{self.dsc[command]['aliases']}```")
+                response_embed.set_footer(text="[] are required command arguments, () are optional command arguments")
+                await ctx.send(embed=response_embed)
+            except KeyError:
+                response_embed = discord.Embed(title=f"**The {command} command sadly doesn't exist!**", color=0xdb2a2a)
+                await ctx.send(embed=response_embed)
 
     @commands.command(aliases=["adminhelp", "admhelp"])
     async def admin_help(self, ctx, command=None):
@@ -60,20 +64,24 @@ class HelpCommand(commands.Cog):
             response_embed.set_footer(text="[] are required command arguments, () are optional command arguments")
             await ctx.send(embed=response_embed)
         else:
-            response_embed = discord.Embed(title=f"**The {command} command:**", color=0x1ced23)
-            args_to_put = ""
-            for arg in self.adm_dsc[command]["args"]:
-                if self.adm_dsc[command]["args"][arg]["required"]:
-                    args_to_put += f" [{arg}]"
-                else:
-                    args_to_put += f" ({arg})"
-            response_embed.add_field(name=f"**Usage:**", value=f"```{command}{args_to_put}```")
-            response_embed.add_field(name=f"**Description:**", value=f"```{self.adm_dsc[command]['desc']}```")
-            response_embed.add_field(name=f"**Required Permissions:**",
-                                     value=f"```Bot Owner```")
-            response_embed.add_field(name=f"**Aliases:**", value=f"```{self.adm_dsc[command]['aliases']}```")
-            response_embed.set_footer(text="[] are required command arguments, () are optional command arguments")
-            await ctx.send(embed=response_embed)
+            try:
+                response_embed = discord.Embed(title=f"**The {command} command:**", color=0x1ced23)
+                args_to_put = ""
+                for arg in self.adm_dsc[command]["args"]:
+                    if self.adm_dsc[command]["args"][arg]["required"]:
+                        args_to_put += f" [{arg}]"
+                    else:
+                        args_to_put += f" ({arg})"
+                response_embed.add_field(name=f"**Usage:**", value=f"```{command}{args_to_put}```")
+                response_embed.add_field(name=f"**Description:**", value=f"```{self.adm_dsc[command]['desc']}```")
+                response_embed.add_field(name=f"**Required Permissions:**",
+                                         value=f"```Bot Owner```")
+                response_embed.add_field(name=f"**Aliases:**", value=f"```{self.adm_dsc[command]['aliases']}```")
+                response_embed.set_footer(text="[] are required command arguments, () are optional command arguments")
+                await ctx.send(embed=response_embed)
+            except KeyError:
+                response_embed = discord.Embed(title=f"**The {command} command sadly doesn't exist!**", color=0xdb2a2a)
+                await ctx.send(embed=response_embed)
 
 
 def setup(client):
