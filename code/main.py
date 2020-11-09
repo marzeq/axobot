a69910e491b09320fce3e86ba27138c0479b82df
marzeq <58303665+marzeq@users.noreply.github.com>
2020-11-09 14:04:32 +0100

    Added admin help command for custom presence

--- code/main.py
+++ code/main.py
@@ -108,6 +108,13 @@ client.admin_command_descriptions = \
             "args": {},
             "desc": "Reruns the program",
             "aliases": ["hr"]
+        },
+        "presence": {
+            "args": {
+                "type": {"required": True},
+                "presence": {"required": True}
+            },
+            "desc": "Changes the bot presence. Set type 0 for watching, 1 for listening, 2 for playing and 3 to reset."
         }
     }
 
