c3df430237d7df4195fa782c559bea3670b7066a
marzeq <58303665+marzeq@users.noreply.github.com>
2020-11-09 14:37:00 +0100

    Changed codestats to botstats so we can include the bot version there

--- code/cogs/botstats.py
+++ code/cogs/botstats.py
@@ -2,14 +2,14 @@ import discord
 from discord.ext import commands
 import os
 
-class CodeStats(commands.Cog):
+class BotStats(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     # Dont translate or add to help command, lets make this a secret
     @commands.command()
-    async def codestats(self, ctx):
+    async def botstats(self, ctx):
         files = 1
         lines = int()
         for file in os.listdir(f"./cogs"):
@@ -20,9 +20,9 @@ class CodeStats(commands.Cog):
                 lines += len(f.readlines())
         with open("main.py") as f:
             lines += len(f.readlines())
-        response_embed = discord.Embed(title=f"Number of files: `{files}`\nNumber of lines of code: `{lines}`")
+        response_embed = discord.Embed(title=f"Current bot version: `{self.client.__version__}`\nNumber of files with code in them: `{files}`\nNumber of lines of code: `{lines}`")
         await ctx.send(embed=response_embed)
 
 
 def setup(client):
-    client.add_cog(CodeStats(client))
+    client.add_cog(BotStats(client))
