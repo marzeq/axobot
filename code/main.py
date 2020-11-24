140c305bcc1fdfe2314b6fe86aa4ce449fe0c730
marzeq <58303665+marzeq@users.noreply.github.com>
2020-11-24 09:16:17 +0100

    Oops forgot to change the version

--- code/main.py
+++ code/main.py
@@ -68,7 +68,7 @@ client.reddit = praw.Reddit(client_id=reddit["id"],
                             client_secret=reddit["secret"],
                             user_agent='RoboMarzeq by u/Marzeq_')
 
-client.__version__ = "0.1.13b"
+client.__version__ = "0.1.14b"
 
 # Admin command descriptions. It's here because it shouldn't be translated
 client.admin_command_descriptions = \
