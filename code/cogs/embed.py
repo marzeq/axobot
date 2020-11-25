cdb7282195c0ccf37a5e1669b7eea270dfeeb2b0
marzeq <58303665+marzeq@users.noreply.github.com>
2020-11-25 10:05:54 +0100

    Editing embeds

--- code/cogs/embed.py
+++ code/cogs/embed.py
@@ -7,7 +7,7 @@ import json
 class Embed(commands.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: commands.Bot = client
 
     @commands.command(aliases=["ebd"])
     async def embed(self, ctx, *, jsonstr: str):
@@ -37,6 +37,53 @@ class Embed(commands.Cog):
             # Creates and sends the created embed
             response_embed = discord.Embed.from_dict(jsondict)
             await ctx.send(embed=response_embed)
+        except:
+            await ctx.send(embed=discord.Embed(
+                title="Something went wrong here! Make sure that this embed is valid! Try checking with the example here!",
+                color=0xdb2a2a
+            ))
+            return
+
+    @commands.command(aliases=["eebd"])
+    async def editembed(self, ctx: commands.Context, id: str, *, jsonstr: str):
+        # # Getting all translations
+        # lang = self.client.get_server_lang(ctx.guild)
+        # useful = lang["translations"]["embed"]
+        try:
+            id = int(id)
+        except ValueError:
+            id = 0
+
+        if id == 0:
+            raise discord.ext.commands.errors.BadArgument(ctx.message)
+
+        try:
+            msg = await ctx.fetch_message(id)
+        except:
+            raise discord.ext.commands.errors.BadArgument(ctx.message)
+
+        if jsonstr.startswith("```json"):
+            jsonstr = jsonstr[7:]
+        elif jsonstr.startswith("```"):
+            jsonstr = jsonstr[3:]
+        if jsonstr.endswith("```"):
+            jsonstr = jsonstr[:-3]
+
+        try:
+            jsondict: dict = json.loads(jsonstr)
+            if jsondict.get("color") is not None:
+                jsondict["color"] = int(jsondict["color"], 16)
+
+        except:
+            await ctx.send(embed=discord.Embed(
+                title="This embeds` code is invalid. Try using the example one here: https://pastebin.com/vX9PVLqf !", color=0xdb2a2a
+            ))
+            return
+
+        try:
+            # Creates and sends the created embed
+            response_embed = discord.Embed.from_dict(jsondict)
+            await msg.edit(embed=response_embed)
         except:
             await ctx.send(embed=discord.Embed(
                 title="Something went wrong here! Make sure that this embed is valid! Try checking with the example here!",
