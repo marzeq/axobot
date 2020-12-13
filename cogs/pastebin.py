7f811aece3abb718dcc41dc5bd35963dc8dffbbb
marzeq <marzeqmarzeq@gmail.com>
2020-12-13 18:17:07 +0100

    The auth I implemented didnt work at all

--- cogs/pastebin.py
+++ cogs/pastebin.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import aiohttp
+import requests
 import json
 
 
@@ -13,34 +14,36 @@ class Pastebin(commands.Cog):
             self.key = cfg["api_key"]
             self.name = cfg["username"]
             self.password = cfg["password"]
+        data = {
+            'api_dev_key': self.key,
+            'api_user_name': self.name,
+            'api_user_password': self.password
+        }
+        request = requests.post(f"https://pastebin.com/api/api_login.php", data=data)
+        if request.status_code != 200:
+            self.ukey = None
+        else:
+            self.ukey = request.text
 
     @commands.command(aliases=["paste"])
     async def pastebin(self, ctx: commands.Context, *, content: str):
         async with aiohttp.ClientSession() as session:
             data = {
-                'api_dev_key': self.key,
-                'api_user_name': self.name,
-                'api_user_password': self.password
-            }
-            async with session.post(f"https://pastebin.com/api/api_login.php", data=data) as r:
-                if r.status != 200:
-                    await ctx.send(embed=discord.Embed(title="Something went wrong here! Contact the developer to resolve this issue!"))
-                    return
-        async with aiohttp.ClientSession() as session:
-            data = {
                 'api_option': 'paste',
                 'api_dev_key': self.key,
                 'api_paste_code': content,
+                "api_user_key": self.ukey,
                 'api_paste_name': f"{ctx.author.name}'s paste",
                 "api_paste_private": 1
             }
+            if self.ukey is None:
+                data.pop("api_user_key")
             async with session.post(f"https://pastebin.com/api/api_post.php", data=data) as r:
                 if r.status == 200:
                     resp = await r.text()
                 else:
                     resp = "Something went wrong here! Contact the developer to resolve this issue!"
         await ctx.send(embed=discord.Embed(title=resp))
-
 
 
 def setup(client):
